bbcb24fe09bc690fb3f51820553d7dab4ba7af28
oleksii.bovsunivskyi <oleksii.bovsunivskyi@omnigon.com>
2020-05-15 14:13:48 +0300

    updating Homework

--- src/test/java/Homework1/Homework1.java
+++ src/test/java/Homework1/Homework1.java
@@ -8,29 +8,19 @@ public class Homework1 {
     System.out.println("Fib: " + fib(10));
   }
 
-  public static int fib(int Fn) {
+  public static int fib(int numFib) {
 
     int first = 0;
     int second = 1;
     int next;
 
-//    int F2 = F1 + F0;  //= 1 + 0 = 1;
-//    int F3 = F2 + F1;  //= 1 + 1 = 2;
-//    int F4 = F3 + F2; //= 2 + 1 = 3;
-//    int F5 = F4 + F3; //= 3 + 2 = 5;
-//    int F6 = F5 + F4; //= 5 + 3 = 8;
-//    int F7 = F6 + F5; //= 8 + 5 = 13;
-//    int F8 = F7 + F6; //= 13 + 8 = 21;
-//    int F9 = F8 + F7; //= 21 + 13 = 34;
-//    int F10 = F9 + F8; //= 34 + 21 = 55;
-//    int F11 = F10 + F9; //= 55 + 34 = 89;
 
-    if (Fn < 0) {
+    if (numFib < 0) {
       System.out.println("Please insert valid value >= 0");
     }
     else {
 
-      for (int n = 0; n < Fn; n++) {
+      for (int n = 0; n < numFib; n++) {
         next = second + first;
         second = first;
         first = next;
